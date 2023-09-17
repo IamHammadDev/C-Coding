07022ff7ad4a5d0372e1a9003256804f5cba7c9a
Hammad <hammadehsan3@gmail.com>
2023-09-18 00:09:32 +0500

    PF PD-02 PACMAN

---
+++ Pacman PD-2/HeaderProject.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"*********************************************";
+	cout<<"\n";
+	cout<<"*                                           *";
+	cout<<"\n";
+	cout<<"*          University Admission             *";
+	cout<<"\n";
+	cout<<"*          Management system                *";
+	cout<<"\n";
+	cout<<"*                                           *";
+	cout<<"\n";
+	cout<<"*                                           *";
+	cout<<"\n";
+	cout<<"*********************************************";
+	return 0;
+}
